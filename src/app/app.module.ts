e96690d3360e42db2bea13375bd229bb7d8a6099
Kaustubh Kuber <kkuber@xpanxion.com>
2020-02-21 12:01:32 +0530

    Initial Commit for PrimeNG Controls repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TreeModule} from 'primeng/tree';
+import {TreeDragDropService} from 'primeng/api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TreeModule
+            
   ],
-  providers: [],
+  providers: [TreeDragDropService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/data-service.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import {TreeModule} from 'primeng/tree';
+import {TreeNode} from 'primeng/api';
+
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getFiles(): Observable<TreeNode[]>  {
+    //return this.http.get<TreeNode[]>('../assets/data/files.json');
+    return this.http.get<TreeNode[]>('../assets/data/templateData.json');                
+                // .pipe(
+                //   tap(res => console.log(JSON.stringify(res))),
+                //   catchError(this.handleError)
+                // );                
+  }
+
+  private handleError(err) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+    console.error(err);
+    return throwError(errorMessage);
+  }
+}
